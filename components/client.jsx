1da513b981a2b80b7fb537fb759241229aa83fb2
Kaustubhya Shukla <kaustubhyaksd@gmail.com>
2024-05-02 22:43:41 +0530

    created some routes

---
+++ components/client.jsx
@@ -0,0 +1,14 @@
+"use client";
+
+const ExampleClient = () => {
+  console.log(
+    "Where do I render***********************😆😆😆😆😆😆😆😆😆😆😆😆😆😆*?"
+  );
+  return (
+    <main>
+      <h2>Hello I am USE CLIENT</h2>
+    </main>
+  );
+};
+
+export default ExampleClient;
---
+++ components/example-server.jsx
@@ -0,0 +1,12 @@
+const ServerClient = () => {
+  console.log(
+    "Where do 🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑I render************************?"
+  );
+  return (
+    <main>
+      <p>Hello I am USE ServerRRRRRRRRRRRRRRRRRRRRRRRRRRRR</p>
+    </main>
+  );
+};
+
+export default ServerClient;
